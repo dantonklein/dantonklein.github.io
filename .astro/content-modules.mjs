834ab8707350b5bc66fb25e5c2d2e02a5399b154
Danton Klein <dantonmklein@gmail.com>
2025-12-03 21:46:01 -0500

    the2

---
+++ .astro/content-modules.mjs
@@ -0,0 +1,7 @@
+
+export default new Map([
+["src/content/blog/embed-examples.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fembed-examples.mdx&astroContentModuleFlag=true")],
+["src/content/blog/guides/content-collections-guide.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fguides%2Fcontent-collections-guide.mdx&astroContentModuleFlag=true")],
+["src/content/blog/guides/markdown-guide.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fguides%2Fmarkdown-guide.mdx&astroContentModuleFlag=true")],
+["src/content/blog/guides/bloomfolio-complete-guide.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fguides%2Fbloomfolio-complete-guide.mdx&astroContentModuleFlag=true")]]);
+		
